9da8ebd83f3c3fc890f5e734129f3e7c3ba3e16b
salmansaleem2 <salmansaleem8523@gmail.com>
2023-07-02 14:48:43 +0500

    added Constructor Functions and the new Operator

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this b/c if we have 1000 object it will create 1000 function it will create serious issue for the performance instead we will use prototypal inheritance
+  this.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+};
+
+const jonas = new Person('Jonas', '1991 ');
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return  {}
+
+const matilda = new Person('matilda', 2017);
+const jack = new Person('Jack', 2017);
+console.log(matilda, jack);
+
+// Never crete a method inside of a constructor fun.
