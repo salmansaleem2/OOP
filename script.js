dc3b141e03a5102eb4172492e38f60d4158da174
salmansaleem2 <salmansaleem8523@gmail.com>
2023-07-05 12:06:16 +0500

    added prototype

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // Never do this b/c if we have 1000 object it will create 1000 function it will create serious issue for the performance instead we will use prototypal inheritance
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 };
 
 const jonas = new Person('Jonas', '1991 ');
@@ -20,6 +20,36 @@ console.log(jonas);
 
 const matilda = new Person('matilda', 2017);
 const jack = new Person('Jack', 2017);
-console.log(matilda, jack);
+// console.log(matilda, jack);
+
+// console.log(matilda instanceof Person);
 
 // Never crete a method inside of a constructor fun.
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+matilda.calcAge();
+jonas.calcAge();
+
+console.log(jonas);
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Home Species';
+console.log(jonas, matilda);
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
