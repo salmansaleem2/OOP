9c84fbd2283d7f50922b0c142a516f00080999db
salmansaleem2 <salmansaleem8523@gmail.com>
2023-07-07 12:33:05 +0500

    added():ES6 classes

--- script.js
+++ script.js
@@ -77,3 +77,34 @@ Array.prototype.unique = function () {
 };
 
 console.log(arr.unique());
+
+// ES6 classes
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('jessica', 1996);
+console.log(jessica);
+
+jessica.calcAge();
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
